049831e93c508541dca947f104a62add3210466b
Maria Bjuvsjö <mariabjuv@gmail.com>
2022-12-14 14:06:47 +0800

    change to maria image

--- update_containers.bash
+++ update_containers.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker pull agnesrodhe/vteam-backend:1.0
+docker pull mabs21/vteam-backend-new:1.0
 docker pull annabeerg/docker-reactjs:latest
 docker pull vince232/mobile-app:latest

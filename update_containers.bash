88205f84450bda15bbd7c72a1ca8c73090a350c8
Maria Bjuvsjö <mariabjuv@gmail.com>
2022-12-20 16:13:13 +0800

    change backend image to latest tag

--- update_containers.bash
+++ update_containers.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker pull mabs21/vteam-backend-new:1.0
+docker pull mabs21/vteam-backend-new:latest
 docker pull annabeerg/docker-reactjs:latest
 docker pull vince232/mobile-app:latest
